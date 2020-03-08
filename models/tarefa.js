83a89ace9d155468f3d03d521b52f789ac86d89e
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2020-03-08 16:14:08 -0300

    Update

--- models/tarefa.js
+++ models/tarefa.js
@@ -1,9 +1,14 @@
 const TIPO_MODIFICACAO = require('../lib/constants').TIPO_MODIFICACAO
 
-const Tarefa = function (numeroTarefa, tipoAlteracao) {
-
+const Tarefa = function ({
+    numeroTarefa,
+    tipoAlteracao,
+    listaArtefato
+}) {
     this.numeroTarefa = numeroTarefa
     this.tipoAlteracao = tipoAlteracao
+    this.listaArtefato = listaArtefato
+
     this.numeroAlteracao = 1
 
     this.isTipoAlteracaoModificacao = () => this.tipoAlteracao === TIPO_MODIFICACAO.MODIFIED
