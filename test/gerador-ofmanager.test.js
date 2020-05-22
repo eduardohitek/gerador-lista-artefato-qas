e43bb6bd32f05b81f73798b19bd9218b81723da4
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2020-05-22 19:25:37 -0300

    Update

--- test/gerador-ofmanager.test.js
+++ test/gerador-ofmanager.test.js
@@ -97,7 +97,7 @@ describe('test gerais', () => {
             new Error(`Projeto ${paramsError.listaProjeto[0]} não encontrado`));
     })
 
-    xit('teste de listagem de artefatos renomeados 2 vezes', async () => {
+    it('teste de listagem de artefatos renomeados 2 vezes', async () => {
 
         await gitUtil.manipularArquivoComCommit('1111111',
             'arquivoFoo.txt', TIPO_MODIFICACAO.ADDED)
@@ -118,26 +118,29 @@ describe('test gerais', () => {
             'arquivoBar.txt', TIPO_MODIFICACAO.MODIFIED)
 
         const lista = await gerador(params).gerarListaArtefato()
+        const printer = require('../lib/printer-ofmanager')(params, lista)
 
-        expect(lista).toHaveLength(2)
+        printer.imprimirListaSaida()
 
-        expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
-        expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
-        expect(lista[0].listaArtefatoSaida).toHaveLength(1)
+        // expect(lista).toHaveLength(2)
 
-        expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe(TIPO_MODIFICACAO.ADDED)
-        expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
-        expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
+        // expect(lista[0].listaNumTarefaSaida).toHaveLength(1)
+        // expect(lista[0].listaNumTarefaSaida[0]).toBe('1111111')
+        // expect(lista[0].listaArtefatoSaida).toHaveLength(1)
 
-        expect(lista[1].listaNumTarefaSaida).toHaveLength(1)
-        expect(lista[1].listaNumTarefaSaida[0]).toBe('1111111')
-        expect(lista[1].listaArtefatoSaida).toHaveLength(1)
+        // expect(lista[0].listaArtefatoSaida[0].tipoAlteracao).toBe(TIPO_MODIFICACAO.ADDED)
+        // expect(lista[0].listaArtefatoSaida[0].numeroAlteracao).toBe(1)
+        // expect(lista[0].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
 
-        expect(lista[1].listaArtefatoSaida[0].tipoAlteracao).toBe(TIPO_MODIFICACAO.RENAMED)
-        expect(lista[1].listaArtefatoSaida[0].numeroAlteracao).toBe(2)
-        expect(lista[1].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
-        expect(lista[1].listaArtefatoSaida[0].nomeAntigoArtefato).toBe('foo/arquivoFoo.txt')
-        expect(lista[1].listaArtefatoSaida[0].nomeNovoArtefato).toBe('foo/arquivoBar.txt')
+        // expect(lista[1].listaNumTarefaSaida).toHaveLength(1)
+        // expect(lista[1].listaNumTarefaSaida[0]).toBe('1111111')
+        // expect(lista[1].listaArtefatoSaida).toHaveLength(1)
+
+        // expect(lista[1].listaArtefatoSaida[0].tipoAlteracao).toBe(TIPO_MODIFICACAO.RENAMED)
+        // expect(lista[1].listaArtefatoSaida[0].numeroAlteracao).toBe(2)
+        // expect(lista[1].listaArtefatoSaida[0].nomeArtefato).toBe('foo/arquivoBar.txt')
+        // expect(lista[1].listaArtefatoSaida[0].nomeAntigoArtefato).toBe('foo/arquivoFoo.txt')
+        // expect(lista[1].listaArtefatoSaida[0].nomeNovoArtefato).toBe('foo/arquivoBar.txt')
     })
 
     xit('teste de listagem de artefato A, R, D e A novamente', async () => {
