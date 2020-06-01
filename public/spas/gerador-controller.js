b4286496c17e0ca84155c78f204cd747504b4584
diegomdrs <33233751+diegomdrs@users.noreply.github.com>
2020-06-01 15:46:13 -0300

    Update

--- public/spas/gerador-controller.js
+++ public/spas/gerador-controller.js
@@ -48,6 +48,38 @@ function GeradorController(FileSaver, Blob, geradorService, blockUI, clipboardUt
             "model": "5 Series"
         }]
 
+        vm.lista = [{
+            tarefa: '1111111',
+            listaArtefato: [
+                {
+                    tipoAlteracao: "A",
+                    nomeArtefato: "A1"
+                },
+                {
+                    tipoAlteracao: "A",
+                    nomeArtefato: "A2"
+                },
+                {
+                    tipoAlteracao: "M",
+                    nomeArtefato: "A2"
+                }
+            ]            
+        }]
+        
+        // {
+        //     tarefa: "1111111",
+        //     tipoAlteracao: 'A'
+        // }, {
+        //     tarefa: "1111111",
+        //     tipoAlteracao: 'A'
+        // }, {
+        //     tarefa: "2222222",
+        //     tipoAlteracao: 'A'
+        // }, {
+        //     tarefa: "2222222",
+        //     tipoAlteracao: 'A'
+        // }]
+
         limparMessages()
         limparFiltros()
 
